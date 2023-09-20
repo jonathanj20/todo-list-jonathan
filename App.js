9d5b2911c9a901efa4eb2fd2ccecf70091981b85
jonathanj20 <jonathanpossada@gmail.com>
2023-09-19 21:50:23 -0700

    se hizo el estilo al componente del login

--- App.js
+++ App.js
@@ -2,15 +2,14 @@ import React from "react";
 import { NativeBaseProvider, Box, Container } from "native-base";
 import CustomInput from "./CustomInput";
 import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
         <Container>
-          <Login />
-          {/* Este componente es solo un ejemplo */}
-          <CustomInput text="Usuario" />
+          <Login />  
         </Container>
       </Box>
     </NativeBaseProvider>
--- Components/Login.js
+++ Components/Login.js
@@ -1,16 +1,28 @@
-import { Text, Image } from "native-base";
+import { Text, Image, Input, Button, Link, extendTheme, View } from "native-base";
 
 export default function Login() {
   return (
-    <>
+    <View background={'#22d3ee'} padding={70}>
     {/* La imagen puede ser una url o el path de una imagen en la carpeta */}
-      <Image
+      <Image 
         source={{
-          uri: "https://wallpaperaccess.com/full/317501.jpg",
+          uri: "https://upload.wikimedia.org/wikipedia/commons/6/67/Microsoft_To-Do_icon.png",
         }}
         alt="Alternate Text"
         size="xl"
+        margin={"auto"}
       />
-    </>
+      <Text fontSize={30} margin={10} color={"white"}>Login</Text>
+      <Text color={"white"} fontWeight={"bold"}>Username</Text>
+      <Input placeholder="Type your username" margin={5} width={"140"}></Input>
+      <Text color={"white"} fontWeight={"bold"}>Password</Text>
+      <Input placeholder="Type your password" type="password" margin={5}></Input>
+      <Button>Login</Button>
+      <Link marginTop={5}>Forgot password?</Link>
+    </View>
   );
 }
+
+
+
+
